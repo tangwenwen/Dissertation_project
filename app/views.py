91196c32261bdd93cb55562101cd350f292dfe10
tww <497159777@qq.com>
2019-06-17 12:48:05 +0800

    update setting

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import StreamingHttpResponse
+
+from functools import wraps
+
+import os
+
+# 主页后台代码
+def index(request):
+    return render(request,'index.html')
