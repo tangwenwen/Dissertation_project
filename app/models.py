b3380948ddd290297be3b7245c593169cf1ebabe
llx-git <873731329@qq.com>
2019-06-20 09:33:50 +0800

    sql

--- app/models.py
+++ app/models.py
@@ -10,17 +10,20 @@ class User_info(models.Model):
     create_time = models.DateTimeField(auto_now=True, verbose_name=u"创建时间")
     user_choices = ((1,u'学生'),(2,u"老师"),(3,u"管理员"))
     comment_type = models.IntegerField(choices=user_choices, default=1, verbose_name=u"用户类型")
-
+    def __str__(self):
+        return self.username
 
 #老师表
 class Teacher_info(models.Model):
-    teacher = models.OneToOneField(to = 'User_info',on_delete=models.CASCADE, verbose_name=u"老师")
+    teacher = models.OneToOneField(to = 'User_info',on_delete=models.CASCADE, verbose_name=u"老师",default='')
     teacher_name = models.CharField(max_length=255, verbose_name=u"教师姓名",default='')
     teacher_school = models.CharField(max_length=255, verbose_name=u"教师学校",default='')
     teacher_major = models.CharField(max_length=255, verbose_name=u"教师专业",default='')
     project_1 = models.OneToOneField(to='project',related_name='project_1', on_delete=models.CASCADE, verbose_name=u"老师项目项目一" ,null=True,blank=True)
     project_2 = models.OneToOneField(to='project', related_name='project_2',on_delete=models.CASCADE, verbose_name=u"老师项目项目二" ,null=True,blank=True)
     project_3 = models.OneToOneField(to='project', related_name='project_3',on_delete=models.CASCADE, verbose_name=u"老师项目项目三" ,null=True,blank=True)
+    def __str__(self):
+        return self.teacher_name
 
 #学生表
 class Student_info(models.Model):
@@ -32,12 +35,15 @@ class Student_info(models.Model):
     student_school = models.CharField(max_length=255, verbose_name=u"学生学校",default='')
     student_class = models.CharField(max_length=255, verbose_name=u"学生所在班级",default='')
     project = models.OneToOneField(to='project',on_delete=models.CASCADE, verbose_name=u"学生项目"  ,null=True,blank=True)
-
+    def __str__(self):
+        return self.student_name
 
 #g管理员表
 class Admin_info(models.Model):
     admin = models.OneToOneField(to='User_info',on_delete=models.CASCADE, verbose_name=u"管理员" )
-
+    admin_name = models.CharField(max_length=255, verbose_name=u"管理员名称",default='')
+    def __str__(self):
+        return self.admin_name
 
 #学生文件表
 class student_file(models.Model):
@@ -48,7 +54,8 @@ class student_file(models.Model):
     student_upload_add = models.CharField(max_length=250, verbose_name=u"学生文件地址", default='')
     student_file_choices = ((1, u'未查阅'), (2, u"已查阅"))
     student_file_flag = models.IntegerField(choices=student_file_choices, default=1, verbose_name=u"文件是否被老师查阅")
-
+    def __str__(self):
+        return self.student_file_name
 
 #消息表
 class message(models.Model):
@@ -57,6 +64,8 @@ class message(models.Model):
     message_replyto = models.ForeignKey('self',related_name="子消息",blank=True,null=True,verbose_name=u"父消息",on_delete=models.CASCADE)
     message_upload_time = models.DateTimeField(auto_now=True, verbose_name=u"消息公告时间")
     message_publisher = models.ForeignKey('User_info', on_delete=models.CASCADE, verbose_name=u"发布消息的人", default='')
+    def __str__(self):
+        return self.project.project_name
 
 #公告表
 class broadcast(models.Model):
@@ -65,6 +74,8 @@ class broadcast(models.Model):
     broadcast_content = models.TextField(verbose_name=u"公告内容",default='')
     broadcast_choices = ((1, u'学生'), (2, u"老师"),(3, u"所有广播"))
     broadcast_upload_to = models.IntegerField(choices=broadcast_choices, default=1, verbose_name=u"向何种用户发布")
+    def __str__(self):
+        return self.manager.admin_name
 
 #项目表
 class project(models.Model):
@@ -74,3 +85,5 @@ class project(models.Model):
     project_enddate = models.DateTimeField(auto_now=False, verbose_name=u"项目结束时间")
     project_choices = ((1,u'未结题'),(2,u"已结题"))
     project_project_isfinished = models.IntegerField(choices=project_choices, default=1, verbose_name=u"项目当前状态")
+    def __str__(self):
+        return self.project_name
